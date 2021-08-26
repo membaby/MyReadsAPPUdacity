036365d2a3af2540678587e3eed98c0d98f52a33
Mahmoud Embaby <44979898+membaby@users.noreply.github.com>
2021-08-26 20:16:26 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -10,14 +10,19 @@ class BooksApp extends React.Component {
 
   // API calls for retrieving books data and updating shelves.
   updateData = () => {BooksAPI.getAll().then(data => {this.setState({books: data})})}
-  refreshBooks = () => {BooksAPI.getAll().then(data => {this.setState({all_books: data})})}
+  // refreshBooks = () => {BooksAPI.getAll().then(data => {this.setState({all_books: data})})}
   refreshShelf = (book, shelf) => {BooksAPI.update(book, shelf).then(response => {this.updateData()})}
+
+  async componentDidMount() {
+    const books = await BooksAPI.getAll()
+    this.setState({all_books: books})
+  }
 
   render() {
     return (
       // The app has two main routes: ./ for main page and ./search for searching page.
       <div className="app">
-        <Route exact path='/' render={() => <MainBooksPage load={this.refreshBooks()} curBooks={this.state.all_books}/>}/>
+        <Route exact path='/' render={() => <MainBooksPage curBooks={this.state.all_books}/>}/>
         <Route path="/search" render={() => <BookSearch updateShelf={this.refreshShelf} curBooks={this.state.all_books}/>}/>
       </div>
     )
